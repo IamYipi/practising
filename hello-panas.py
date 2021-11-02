3d2b167048b4547ff4fdf35d523c2b5c6806f807
Javier Garcia <93437997+IamYipi@users.noreply.github.com>
2021-11-02 14:26:31 +0100

    Create hello-panas.py

---
+++ hello-panas.py
@@ -0,0 +1,16 @@
+#ARCHIVO EN PYTHON PARA PRACTICAR COMMITS Y PUSH,PULLS,ETC
+#Definimos funcion main
+def main():
+  yipi()
+
+
+#FUNCION CREADA POR IAMYIPI
+def yipi():
+  print("Hola chicos, soy IamYipi")
+  print("Escribid por debajo lo que querais, un saludo")
+
+
+
+#main
+if __name__ == "__main__":
+  main()
